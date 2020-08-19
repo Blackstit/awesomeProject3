f8cfa955472304e85c74a88b5482737e856fa7af
Vladislav Petrushkin <56411253+Blackstit@users.noreply.github.com>
2020-08-19 19:20:32 +0200

    Update config.go

--- config.go
+++ config.go
@@ -1,9 +1,9 @@
 package main
 
 const (
-	vkServiceKey = "77fd337377fd337377fd337397778eb76d777fd77fd337328c670c1009d987f845989fb"
+	vkServiceKey = ""
 	version      = "5.122"
 	reqURL       = "https://api.vk.com/method/wall.get?"
-	webHook      = "https://blackstitgolangbot.herokuapp.com/"
-	tgToken      = "1394276809:AAHV7J_8-S6zwPErKOUO9ZRqzMEu5TS7e3Y"
+	webHook      = ""
+	tgToken      = ""
 )
